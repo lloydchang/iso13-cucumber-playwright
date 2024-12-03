628abc01f9fd5f06a9ad91297f73e29b9bb9194a
David Tran <davidtranbill@gmail.com>
2024-12-02 19:23:00 -0600

    add environment constant

--- src/steps/env.steps.ts
+++ src/steps/env.steps.ts
@@ -1,12 +1,10 @@
 import { Given } from '@cucumber/cucumber';
 import { CustomWorld } from '../support/world';
 import { environments } from '../support/environments';
-import { browser } from '@tensorflow/tfjs';
 
 Given('I navigate to homepage', async function (this: CustomWorld) {
   const envKey = process.env.ENV as keyof typeof environments || 'qa';
   const baseURL = environments[envKey];
-  //if (!baseURL) throw new Error('Invalid environment value');
-  await this.page?.goto(baseURL); // Navigate to the base URL
+  await this.page?.goto(baseURL); 
   await this.page?.waitForTimeout(5000);
 });
